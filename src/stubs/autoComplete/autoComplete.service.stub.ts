2f8dcffeac72c144024e0037a000f9d5e2c2bccf
Tshepo Tsilo <tshepo.tsilo@binaryits.co.za>
2022-11-13 23:38:54 +0200

    test: create autoComplete service stub

---
+++ src/stubs/autoComplete/autoComplete.service.stub.ts
@@ -0,0 +1,46 @@
+import { HttpException } from "../../common/httpException";
+import { config } from "../../config";
+import { IAutoCompleteService } from "../../declarations/interfaces";
+import { Place, AutoCompleteResult } from "../../declarations/types";
+import data from "../../stubs/data/autoComplete.response.json";
+
+const baseURL: string = config.envVars.api.map.baseURL || "";
+
+export default class AutoCompleteServiceStub implements IAutoCompleteService {
+	#baseURL = baseURL;
+
+	public static getPlaces = async function (query: string) {
+		// 1. Set up request config
+
+		// 2. Attempt to make the request
+
+		// 2.1 Throw error if request attempt fails
+
+		// 3. Return result
+		let result: AutoCompleteResult;
+		result = await new Promise<AutoCompleteResult>((resolve, reject) => {
+			let results: Place[] = [];
+			const test = setTimeout(() => {
+				let apiResult: AutoCompleteResult = data;
+				data.result.map((place) => {
+					if (
+						place.title.toLowerCase().includes(query.toLowerCase()) ||
+						place.description.toLowerCase().includes(query.toLowerCase())
+					) {
+						results.push(place);
+					}
+				});
+
+				if (results.length > 0) {
+					apiResult.result = results;
+					resolve(apiResult);
+				} else {
+					const error = new HttpException("NotFound", "No results found");
+					reject(error);
+				}
+			}, 1000);
+		});
+
+		return result;
+	};
+}
