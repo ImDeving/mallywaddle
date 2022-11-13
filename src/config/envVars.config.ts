0a510622cb7f049a7e4199bba69475580c0a495d
Tshepo Tsilo <tshepo.tsilo@binaryits.co.za>
2022-11-14 01:08:17 +0200

    feat: add implementation of autoComplete api

--- src/config/envVars.config.ts
+++ src/config/envVars.config.ts
@@ -19,11 +19,15 @@ const SERVER = {
 const API_BASE_URL = process.env.API_BASE_URL;
 const API_KEY_NAME = process.env.API_KEY_NAME;
 const API_KEY_VALUE = process.env.API_KEY_VALUE;
+const API_AC_PATH = process.env.API_AC_PATH;
+const API_GC_PATH = process.env.API_GC_PATH;
 const API = {
 	map: {
 		baseURL: API_BASE_URL,
 		apiKeyName: API_KEY_NAME,
 		apiKeyValue: API_KEY_VALUE,
+		autoCompletePath: API_AC_PATH,
+		geoCodePath: API_GC_PATH,
 	},
 };
 
--- src/services/autoComplete.service.ts
+++ src/services/autoComplete.service.ts
@@ -1,46 +1,52 @@
+import needle from "needle";
 import { HttpException } from "../common/httpException";
 import { config } from "../config";
 import { IAutoCompleteService } from "../declarations/interfaces";
 import { Place, AutoCompleteResult } from "../declarations/types";
 import data from "../stubs/data/autoComplete.response.json";
 
-const baseURL: string = config.envVars.api.map.baseURL || "";
+const baseURL = config.envVars.api.map.baseURL || "";
+const autoCompletePath = config.envVars.api.map.autoCompletePath || "";
+const apiKeyName = config.envVars.api.map.apiKeyName || "";
+const apiKeyValue = config.envVars.api.map.apiKeyValue || "";
 
 export default class AutoCompleteService implements IAutoCompleteService {
 	#baseURL = baseURL;
 
-	public static getPlaces = async function (query: string) {
+	public static getPlaces = async (query: string) => {
 		// 1. Set up request config
+		const token =
+			"eyJraWQiOiJXcURTMHRaZ2lQb1wvZnVVWVhnaEVYK21USXRLN25oOFBtT0U1eWw2Zmk4Zz0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIzbHJ1Z3MyaHM0OW84dHZobWRlcHVlNDExdCIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoicGxhY2VzXC9nZW9jb2RlLXJlYWQgcGxhY2VzXC9hdXRvY29tcGxldGUtcmVhZCBwbGFjZXNcL2RldGFpbHMtcmVhZCIsImF1dGhfdGltZSI6MTY2ODM3ODE4NSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLmV1LXdlc3QtMS5hbWF6b25hd3MuY29tXC9ldS13ZXN0LTFfVmVucmxGMGxDIiwiZXhwIjoxNjY4MzgxNzg1LCJpYXQiOjE2NjgzNzgxODUsInZlcnNpb24iOjIsImp0aSI6Ijg0MjNmYTM3LTI0NTEtNDU5Yi1hNTU4LWJlMTQyOTNmNDc3OSIsImNsaWVudF9pZCI6IjNscnVnczJoczQ5bzh0dmhtZGVwdWU0MTF0In0.HHnCGoefJ732n_07hIwirv_YyFj_lAxrDWDiNIYqtISvvRJGxYb52NBcOexuqfb0-9S_tg7Yj3CHPz8ZeoBXWTwvEYJRiv6PVFnRH6jeiV-n3jMEoDdwHdE5e80EG44cGWih8xyOOO8WXI75M46Vbli0O9vTVAClmsp5fyMKR1nEyaK4vtkznbgfC1kWTb-T4VyUNSzz86_ai91UaYxplNX_j3Zp4y-GRDCMfqLMGLFL9MWsCi97lVDXy8ppLgveoaMckvrrZHVn1lrJXNKN1heto5uBc6O9rdH22F9M0NzLdPF-OY5b_3vDo7uwwCFZu5wHVCZvuiiAC2Pou5MTeA";
+		const headers: { [key: string]: string } = {};
+		headers[`${apiKeyName}`] = apiKeyValue;
+		headers["Authorization"] = `Bearer ${token}`;
+		const url = `${baseURL}${autoCompletePath}?query=${query}`;
+
+		const reqConfig = {
+			headers,
+		};
+
+		console.log({ reqConfig });
 
 		// 2. Attempt to make the request
+		const apiRes = await new Promise<AutoCompleteResult>((resolve, reject) => {
+			needle.request("get", url, { query }, { headers }, (err, resp) => {
+				if (!err && resp.statusCode == 200) {
+					// Successfull API response
 
-		// 2.1 Throw error if request attempt fails
+					console.log({ Successfull: resp.body });
 
-		// 3. Return result
-		let result: AutoCompleteResult;
-		result = await new Promise<AutoCompleteResult>((resolve, reject) => {
-			let results: Place[] = [];
-			const test = setTimeout(() => {
-				let apiResult: AutoCompleteResult = data;
-				data.result.map((place) => {
-					if (
-						place.title.toLowerCase().includes(query.toLowerCase()) ||
-						place.description.toLowerCase().includes(query.toLowerCase())
-					) {
-						results.push(place);
-					}
-				});
-
-				if (results.length > 0) {
-					apiResult.result = results;
-					resolve(apiResult);
-				} else {
-					const error = new HttpException("NotFound", "No results found");
-					reject(error);
+					// 3. Return result
+					return resolve(resp.body);
 				}
-			}, 1000);
+
+				// Throw an error from the API
+				console.log({ APIError: err });
+				const error = new HttpException("BadRequest", err?.message);
+				return reject(error);
+			});
 		});
 
-		return result;
+		return apiRes;
 	};
 }
